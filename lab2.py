58cea3e4c0d3a913a3972456610ffacfe73656bf
samiyah beaufort <miyah@samiyahs-MacBook-Pro.local>
2025-07-08 19:29:16 -0700

    finished lab2

---
+++ lab2.py
@@ -0,0 +1,21 @@
+
+# Lab 2 - Miyah 
+
+# Step 1
+name = 'Samiyah'
+
+# Step 2
+nickname = 'Miyah'
+
+#Step 3
+age = 16.0
+
+#Step 4 
+used_python = False
+
+#Step 5
+hobbies = ['painting', 'cleaning', 'going out']
+
+#Step 6
+favorite_things = {'pet': 'lofi' , 'food' : 'seafood' , 'color': 'green'}
+
